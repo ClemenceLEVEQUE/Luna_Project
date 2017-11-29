53f8bf8b23ab716b6bee0a05e980e67dd830bca0
Karim <elkoulalikarim@gmail.com>
2017-11-29 13:53:13 +0100

    Voila le debut de l'Interface

---
+++ Maquettes/com/luna/Graphique/InterfaceConnect.java
@@ -0,0 +1,49 @@
+package com.luna.Graphique;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.luna.Graphique.*;
+
+public class InterfaceConnect extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceConnect frame = new InterfaceConnect();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceConnect() {
+		
+		
+		setTitle("Bienvenue sur SARL Luna");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(InterfaceConnect.class.getResource("\\src\\main\\resources\\Moon-32.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
